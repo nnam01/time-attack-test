a8a7ffd766940b3f01cd5f4726f401868de54270
nnam01 <isthenam@naver.com>
2025-12-10 16:23:18 +0900

    fix: update import statement for Param annotation in EventRepository

--- src/main/java/com/nnam01/study/repository/EventRepository.java
+++ src/main/java/com/nnam01/study/repository/EventRepository.java
@@ -1,11 +1,11 @@
 package com.nnam01.study.repository;
 
 import com.nnam01.study.domain.Event;
-import io.lettuce.core.dynamic.annotation.Param;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
